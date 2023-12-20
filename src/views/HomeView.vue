aa92dd3926055f6d2c7ac7f91a762673de9e673a
tornike gogilashvili <tornikegogilashvili@yahoo.com>
2023-12-20 19:54:47 +0400

    vue start

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <nav class="navbar bg-body-tertiary">
+  <div class="container " style="display: flex; justify-content: space-around;">
+    <a class="navbar-brand" href="#">
+      <img src="" alt="Bootstrap" width="30" height="24">
+    </a>
+    <a>შესვლა</a>
+  </div>
+</nav>
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
